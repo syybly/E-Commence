8985bac11f7ce382c23cd2c495f61a02a10f86c0
Sylvia <shaoyueyingswc@gmail.com>
2023-10-20 17:04:41 +1100

    Add project files.

---
+++ E-Commence APP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Commence_APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
